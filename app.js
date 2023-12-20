9c5c95cc557f1ee7f3c776b61ceb53e8f998cbe5
dzakwanIrfan <lexdani368@gamil.com>
2023-12-20 13:42:07 +0700

    menambahkan middleware

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 var expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
 // gunakan ejs
 app.set('view engine', 'ejs');
+
+// third-party middleware
 app.use(expressLayouts);
+app.use(morgan('dev'));
+
+//built-in middleware
+app.use(express.static('public'));
+
+//application level middleware
+app.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  next();
+});
 
 app.get('/', (req, res) => {
   // res.sendFile('./index.html', {root: __dirname});
